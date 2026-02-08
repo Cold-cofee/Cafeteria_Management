d4369c478e1adbae18176151dea5107a1008f575
Cold-cofee <pancir008@gmail.com>
2026-02-08 14:59:06 +0300

    Revert "Экран выдачи"

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, redirect, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-from src.config import app, db, login_manager
+
+from src.config import app, db
 from src.database.users import User
 from src.database.wallets import Wallet
 from src.database.history import History
@@ -9,15 +10,6 @@ from src.database.menu import Menu
 from src.database.store import Storage
 from src.database.reviews import Reviews
 from src.database.requests import Requests
-from src.router import wallet_bp 
-#нужен импорт регистрации
-login_manager.init_app(app)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 
 if __name__ == "__main__":
     # Получаем путь к базе данных из конфига
@@ -33,8 +25,3 @@ if __name__ == "__main__":
     else:
         with app.app_context():
             db.create_all()
-
-    app.register_blueprint(wallet_bp)#регистрация блюпринта
-
-    print('Запуск сервера...')
-    app.run(debug=True)
--- src/auth.py
+++ src/auth.py
@@ -1,34 +1,7 @@
-from flask import Flask, flash, redirect, url_for
-from config import app, db
-from database.users import User
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import login_user
+from flask import flask
 
-def login_user_db(login, password):
-    user = User.query.filter_by(login=login).first()
-    if user and check_password_hash(user.password, password):
-        login_user(user)
-        return True
-    return False
+app = flask(__name__)
 
-def register_user(login, password, password_repeat):
-    existing_user = User.query.filter_by(login=login).first()
-    if existing_user:
-        print('Имя пользователя уже занято')
-        return False
-    elif password != password_repeat:
-        print('Пароли не совпадают')
-        return False
-    else:
-        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
-        new_user = User(login=login, password=hashed_password, role="student", wallet=None)
+app.config['SECRET_KEY'] = 'your-secret-key'
 
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            print('Регистрация прошла успешно! Теперь войдите.')
-            return new_user
-        except Exception as e:
-            db.session.rollback()
-            print('Произошла ошибка при сохранении в базу.')
-            return False
+@app.toute("/")
